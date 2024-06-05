cb91478db4b195fa2dc6f62b3ea72f90dcc86a40
Nayan Thacker <157197883+Nayan1729@users.noreply.github.com>
2024-06-05 14:46:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import axios from "axios";
-import bodyParser from "body-parser";
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
